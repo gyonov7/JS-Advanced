1a7cca2c9e8bbe1e5aa0d0df61160358da5e9b36
gyonov7 <103579725+gyonov7@users.noreply.github.com>
2022-05-21 18:21:20 +0300

    new commit

--- Programming with JS/Advanced/Syntax, Functions and Statements/06. Sum of Numbers N…M.js
+++ Programming with JS/Advanced/Syntax, Functions and Statements/06. Sum of Numbers N…M.js
@@ -1,12 +1,11 @@
-function sumOfNum (startNum, endNum ){
-    startNum = Number(startNum);
-    endNum = Number(endNum);
-let sum = 0;
-    for (let index = startNum; index <= endNum; index++) {
-        sum+=index;
-        
-    }
-    console.log(sum);
+function sumOfNum(startNum, endNum) {
+  startNum = Number(startNum);
+  endNum = Number(endNum);
+  let sum = 0;
+  for (let i = startNum; i <= endNum; i++) {
+    sum += index;
+  }
+  console.log(sum);
 }
-sumOfNum('1', '5' );
-sumOfNum('-8', '20' );
+sumOfNum("1", "5");
+sumOfNum("-8", "20");
