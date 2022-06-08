9dcfa63400b0c9f10c2239bf9487f78e41e9b562
gyonov7 <103579725+gyonov7@users.noreply.github.com>
2022-06-08 12:03:29 +0300

    first exercise

--- Programming with JS/Advanced/DOM Introduction - Exercise/01. Subtraction/subtract.js
+++ Programming with JS/Advanced/DOM Introduction - Exercise/01. Subtraction/subtract.js
@@ -1,3 +1,8 @@
 function subtract() {
-    console.log('TODO:...');
+    let firstNum = document.getElementById('firstNumber').value;
+    let secondNum = document.getElementById('secondNumber').value;
+
+    let result = document.getElementById('result');
+
+    result.textContent = Number(firstNum)-Number(secondNum);
 }
